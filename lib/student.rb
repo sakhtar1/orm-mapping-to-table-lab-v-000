37428cebbd81e80466b9c85b10a0f746028172e7
Sharmina Akhtar <sakht318@gmail.com>
2018-02-15 14:00:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,7 @@ class Student
   def Student.drop_table
 
     drop_table :students
-    DB[:conn].execute(sql
+    DB[:conn].execute(sql)
 
   end
   # Remember, you can access your database connection anywhere in this class
